c10022bb0e49363c45cc1542d48e77434733b4eb
unknown <haren111990@gmail.com>
2025-12-21 14:13:52 +0530

    card bg add

--- app/maincard/page.tsx
+++ app/maincard/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 import { useContext } from 'react'
 import { UserContext } from '@/contextapi/contextAPI'
+import { motion } from 'framer-motion'
 const MainCard = () => {
   const {name,gender}=useContext(UserContext)
   return (
-    <div>
+    <motion.div className="bg-[url('/Images/card/bg2.jpg')] bg-center bg-cover min-h-screen w-full flex flex-col items-center"
+    initial={{y:-800,opacity:0}}
+    animate={{y:0,opacity:1}}
+    transition={{duration:1, ease:'easeOut'}}
+    >
       <h2>{name}</h2>
       <h2>{gender}</h2>
-    </div>
+    </motion.div>
   )
 }
 
