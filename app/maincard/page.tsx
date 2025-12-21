d414afe0e27a1b8cb342093244a34902e63f237c
unknown <haren111990@gmail.com>
2025-12-21 14:16:35 +0530

    update scroll time

--- app/maincard/page.tsx
+++ app/maincard/page.tsx
@@ -8,7 +8,7 @@ const MainCard = () => {
     <motion.div className="bg-[url('/Images/card/bg2.jpg')] bg-center bg-cover min-h-screen w-full flex flex-col items-center"
     initial={{y:-800,opacity:0}}
     animate={{y:0,opacity:1}}
-    transition={{duration:1, ease:'easeOut'}}
+    transition={{duration:3, ease:'easeOut'}}
     >
       <h2>{name}</h2>
       <h2>{gender}</h2>
