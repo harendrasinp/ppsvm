814b847fb23978e5d2e572686d8c5a7539d0171f
unknown <haren111990@gmail.com>
2025-12-27 19:20:28 +0530

    update

--- app/maincard/components/footer.tsx
+++ app/maincard/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="absolute bottom-5 w-full flex justify-center z-50" >
+        <div className="absolute bottom-12 w-full flex justify-center z-50" >
             <div className="text-center text-amber-50 text-[0.6rem]">
                 <div>© 2025 VayuSoftwares</div>
                 <div>Designed & Developed by VayuSoftwares</div>
