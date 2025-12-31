cb6a150a3858be8aa17a2597a5194ca248a24c5f
unknown <haren111990@gmail.com>
2025-12-31 22:50:26 +0530

    update letter

--- app/maincard/page.tsx
+++ app/maincard/page.tsx
@@ -74,7 +74,7 @@ const MainCard = () => {
             {prefix} {name},
           </h1>
 
-          <p className="mt-1 text-sm font-semiboldtext-yellow-950 cursive-text">
+          <p className="mt-1 text-sm font-semibold text-amber-100 cursive-text">
             You are cordially invited to attend the Annual Function of
             P. P. Savani Vidhyamandir. Your gracious presence will make
             the occasion truly special and memorable.
